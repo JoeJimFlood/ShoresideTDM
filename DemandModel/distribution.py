30638796774db15be2292017d8cd4572602edac1
JoeJimFlood <floodjj@uw.edu>
2017-08-26 13:12:19 -0400

    Added Office trips and capability of EE trips

--- DemandModel/distribution.py
+++ DemandModel/distribution.py
@@ -9,6 +9,7 @@ BASE_PATH = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
 OUTPUT_PATH = os.path.join(BASE_PATH, 'Output')
 GENERATION_OUTPUT = os.path.join(OUTPUT_PATH, 'generation_output.csv')
 ATTRACTIONS_FILE = os.path.join(BASE_PATH, r'Network\attractions.csv')
+NODE_INFO_FILE = os.path.join(BASE_PATH, r'Network\node_info.csv')
 
 def normalize(array):
     '''
@@ -28,6 +29,9 @@ def normalize(array):
 
 nodes = pd.read_csv(GENERATION_OUTPUT, index_col = 0).fillna(0)
 attractions = pd.read_csv(ATTRACTIONS_FILE, index_col = 0)
+node_info = pd.read_csv(NODE_INFO_FILE, index_col = 0).fillna(0)
+external_nodes = node_info[node_info['Type'] == 4]
+external_nodes['Percent'] = normalize(external_nodes['Area'])
 
 N = nodes.shape[0] #Number of nodes
 
@@ -38,6 +42,13 @@ for purpose in attractions.columns:
     trip_table = pd.DataFrame(np.zeros((N, N)), nodes.index, nodes.index)
     for node in trip_table.columns:
         trip_table[node] = nodes[purpose + 'Productions'] * attraction_shares[node]
+
+    EE = attractions.loc[0, purpose]
+    for onode in external_nodes.index:
+        for dnode in external_nodes.index:
+            ee_trips = EE*external_nodes.loc[onode, 'Percent']*external_nodes.loc[dnode, 'Percent']
+            print(onode, dnode, ee_trips)
+            trip_table.loc[onode, dnode] += ee_trips
 
     trip_table.to_csv(os.path.join(OUTPUT_PATH, purpose + '_trip_table.csv'))
 
